6c784ac3bc192c8576bafc98aa1f8ee968c30961
hobosock <76829496+hobosock@users.noreply.github.com>
2023-06-14 21:55:44 -0400

    jk haven't generated players

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ use std::fs;
 
 fn main() {
     // quick test, just print out each step of a game and see if it makes sense
-    // 1. load home/away/ballpark files
+    // 1. generate 2 new teams
+    //gen_team(Era::Modern, starters, bench, pitchers, bullpen, name, firstnames, lastnames, logos, mascots, mottos, personalities, backgrounds, locations, name1, name2);
 }
